e9236c4dd46b9cb840fd90b92b3cc33da5e3e1d8
Rogerio <ragranato@gmail.com>
2018-06-02 17:09:05 -0300

    Adding Preco

---
+++ src/main/java/br/com/casadocodigo/loja/models/Preco.java
@@ -0,0 +1,30 @@
+package br.com.casadocodigo.loja.models;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Embeddable
+public class Preco {
+
+	@Column(precision = 2)
+	private BigDecimal valor;
+	@Column
+	@Enumerated(EnumType.STRING)
+	private TipoLivro tipo;
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public TipoLivro getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoLivro tipo) {
+		this.tipo = tipo;
+	}
+}
---
+++ src/main/java/br/com/casadocodigo/loja/models/TipoLivro.java
@@ -0,0 +1,5 @@
+package br.com.casadocodigo.loja.models;
+
+public enum TipoLivro {
+  EBOOK,IMPRESSO,COMBO;
+}
