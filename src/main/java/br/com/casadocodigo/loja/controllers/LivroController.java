3ba37e8cc75d2b023816ae37fc78d9506fe7b91f
Rogerio <ragranato@gmail.com>
2018-06-02 17:10:09 -0300

    Adding Preco

--- src/main/java/br/com/casadocodigo/loja/controllers/LivroController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/LivroController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.LivroDAO;
 import br.com.casadocodigo.loja.models.Livro;
+import br.com.casadocodigo.loja.models.TipoLivro;
 
 @Controller
 public class LivroController {
@@ -22,12 +24,21 @@ public class LivroController {
 		this.livroDAO = livroDAO;
 	}*/
 
-	@RequestMapping("livro/form")
-	public String formulario()
-	{
-		return "livro/form";
-	}
+//	@RequestMapping("livro/form")
+//	public String formulario()
+//	{
+//		return "livro/form";
+//	}
 
+	@RequestMapping("livro/form")
+	public ModelAndView formulario()
+	{
+		ModelAndView view = new ModelAndView(/*viewName: */"livro/form");
+		view.addObject(/*attributeName: */"tiposLivros", TipoLivro.values());
+		
+		return view;
+	}
+	
 	@Transactional
 	@RequestMapping("livro/salva")
 	//public String salvarLivro(String titulo, String autor, Integer numPaginas)
--- src/main/webapp/WEB-INF/views/livro/form.jsp
+++ src/main/webapp/WEB-INF/views/livro/form.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -27,6 +28,17 @@
     <div>
       <input type="submit" value="Salvar"/>
     </div>
+    
+    <c:forEach items="${tiposLivro}" var="tipo" varStatus="status">
+      
+	    <div>
+	      <label>${tipo}</label>
+	      <input type="text" name="precos[${status.index}].valor"/>
+          <input type="hidden" name="precos[${status.index}].tipo" value="${tipo}"/>
+
+	    </div>
+    
+    </c:forEach>
         
   </form>
 
