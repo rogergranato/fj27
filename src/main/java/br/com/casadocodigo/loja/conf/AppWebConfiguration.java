53bbb2af06e37d97a7658b4b847d47e83c0e0575
Rogerio <ragranato@gmail.com>
2018-06-23 10:38:13 -0300

    multiplos formatos

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -16,12 +18,15 @@ import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -30,6 +35,7 @@ import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.daos.LivroDAO;
 import br.com.casadocodigo.loja.infra.GerenciadorDeArquivo;
 import br.com.casadocodigo.loja.models.ShoppingCart;
+import br.com.casadocodigo.loja.resolvers.JSONViewResolver;
 
 // Habilita o component MVC do Spring
 @EnableWebMvc
@@ -97,6 +103,20 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	public RestTemplate criaRestTemplate()
 	{
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public ViewResolver contentNegViewResolver(ContentNegotiationManager manager)
+	{
+		List<ViewResolver> resolvers = new ArrayList<>();
+		resolvers.add(internalResolver());
+		resolvers.add(new JSONViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		
+		return resolver;
 	}
 	
 	// habilita nosso cache manager
